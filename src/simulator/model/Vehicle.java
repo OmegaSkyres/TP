991f60829586a040e3689d6af5b8a4ead901e5fc
Rober <omegaskyres9@gmail.com>
2020-02-13 09:32:53 +0100

    1er Commit

---
+++ src/simulator/model/Vehicle.java
@@ -0,0 +1,61 @@
+package simulator.model;
+
+import org.json.JSONObject;
+import simulator.exceptions.WrongValuesException;
+import simulator.model.SimulatedObject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class Vehicle<Junction> extends SimulatedObject {
+    protected List<Junction> itenerario;
+    protected int velocidadMaxima; // velocidad maxima
+    protected int velocidadActual; // velocidad actual
+    protected VehicleStatus estado; //estado
+    protected Road carretera; //carretera en la que se encuentra el vehiculo
+    protected int localizacion; //distancia desde que comenzo la carretera
+    protected int gradoContaminacion; //numero entre 0 y 10
+    protected int contaminacionTotal; //total emitido
+    protected int kilometraje; // distancia recorrida
+
+    public Vehicle(String id, int velocidadMaxima, int contClass, List<Junction> itinerario) throws WrongValuesException { //Preguntar que es contClass
+        super(id);
+        Collections.unmodifiableList(new ArrayList<>(itinerario)); // creamos la copia aqui??
+        if(!checkValores(velocidadMaxima,gradoContaminacion,itinerario)){
+            throw new WrongValuesException("Los valores introducidos son erroneos");
+        }
+        else{
+
+        }
+
+    }
+
+    @Override
+    protected void advance(int time) {
+        if(estado == VehicleStatus.TRAVELING){
+            localizacion = min((localizacion + velocidadActual),carretera.size()); //Preguntar que funcion min para importar
+        }
+
+    }
+
+    @Override
+    public JSONObject report() {
+        return null;
+    }
+
+    protected boolean checkValores(int velocidadMaxima, int contClass, List<Junction> itinerario){
+        if(velocidadMaxima > 0 && contClass > 0 && contClass < 10 && itinerario.size() >= 2) return true;
+        else return false;
+    }
+
+    protected void setSpeed(int s){
+        velocidadActual = s;
+    }
+
+    protected void setContaminationClass(int c){
+        gradoContaminacion = c;
+    }
+
+}
